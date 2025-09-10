befb5fe8a33609212df34cbf10b7e9c524787e51
ary-chaurasia <aryanchaurasia033@gmail.com>
2025-09-10 23:01:07 +0530

    add user account management routes: change password, get current user details, update account, and manage avatars

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,5 +1,17 @@
 import { Router } from "express";
-import { registerUser,loginUser,logoutUser, refreshAccessToken} from "../controllers/user.controller.js";
+import {
+     registerUser,
+     loginUser,
+     logoutUser,
+     refreshAccessToken,
+     updateAccountDetails,
+     changeCurrentPassword,
+     getCurrentUserDetails,
+     updateUserAvatar,
+     updateUserCoverImage,
+     getUserChannelProfile,
+     getWatchHistory
+} from "../controllers/user.controller.js";
 import { upload } from "../middlewares/multer.middleware.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
 const router = Router();
@@ -19,5 +31,13 @@ router.route("/logout")
 router.route("/refresh-access-token")
  .post( refreshAccessToken); //http://localhost:5000/api/v1/users/refresh-access-token(post)
 
+router.route("/change-password").post(verifyJWT, changeCurrentPassword); //http://localhost:5000/api/v1/users/change-password(post)
+router.route("/current-user").get(verifyJWT, getCurrentUserDetails); //http://localhost:5000/api/v1/users/current-user(get)
+router.route("/update-account").patch(verifyJWT,updateAccountDetails);//http://localhost:5000/api/v1/users/update-account(patch)
+router.route("/avatar-change").patch(verifyJWT, upload.single('avatar'), updateUserAvatar);//http://localhost:5000/api/v1/users/avatar-change(patch)
+router.route("/cover-image-change").patch(verifyJWT, upload.single('/coverImage'), updateUserCoverImage);//http://localhost:5000/api/v1/users/cover-image-change(patch)
 
+router.route("/c/:username").get(verifyJWT, getUserChannelProfile); //http://localhost:5000/api/v1/users/c/:username(get) to get user channel profile 
+
+router.route("/watch-history").get(verifyJWT, getWatchHistory); //http://localhost:5000/api/v1/users/watch-history(get) to get user watch history
 export default router;
